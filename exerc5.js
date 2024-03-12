2db116ed6e92ede22a1eef9bf04bfd13d7e20b24
Júlia Gurnieri <juliaguarnieri04@gmail.com>
2024-03-12 08:30:13 -0300

    Exercício 5 realizado em sala de aula, no qual aborda a idade da pessoa, maior de 18 anos é acesso concedido e menor acesso restrito.

---
+++ exerc5.js
@@ -0,0 +1,11 @@
+//Iniciar idade
+let idade = 23;
+
+//Fazer a condicao: SE ou IF
+if(idade >= 18){
+    // exiba acesso concedido
+    console.log("Acesso concedido");
+}else{
+    //exiba acesso restrito
+    console.log("Não é aceito menor de idade");
+}
