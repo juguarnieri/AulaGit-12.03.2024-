07302b0b480aec4d03af1a1add46ad7ce215f8f9
Júlia Gurnieri <juliaguarnieri04@gmail.com>
2024-03-12 11:20:57 -0300

    mudança valor

--- exerc5.js
+++ exerc5.js
@@ -1,11 +1,11 @@
 //Iniciar idade
-let idade = 12;
+let idade = 23;
 
 //Fazer a condicao: SE ou IF
 if(idade >= 18){
     // exiba acesso concedido
     console.log("Acesso concedido");
 }else{
-    //exiba acesso restrito
+    //exiba acesso restritog
     console.log("Não é aceito menor de idade");
 }
